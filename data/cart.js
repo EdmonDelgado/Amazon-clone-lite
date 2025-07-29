6ca2d632370cf9dd07a9c743f47b0902e772f241
Edmon Delgado <edmondelgado06@gmail.com>
2025-07-28 21:08:19 -0400

    move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,31 @@
 export let cart = [];
+
+export function addToCart(productId) {
+  let matchingItem;
+
+  let quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+  let quantity = Number(quantitySelector.value);
+      
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productName) {
+      matchingItem = cartItem;
+    };
+  });
+
+  if(matchingItem) {
+    matchingItem.quantity += quantity;
+  } else {
+    cart.push({
+      productId,
+      quantity
+    });
+  };
+
+  let addedElement = document.querySelector(`.js-added-to-cart-${productId}`);
+
+  addedElement.classList.add('js-added');
+
+  setTimeout(() =>{
+    addedElement.classList.remove('js-added');
+  }, 2000)
+};
