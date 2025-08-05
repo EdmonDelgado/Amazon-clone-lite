bdbc1b906b6fa68e8041d21ebf214b95c660a16c
Edmon Delgado <edmondelgado06@gmail.com>
2025-08-04 21:12:28 -0400

    save cart to localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,14 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) || [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
 }, {
   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity: 1
 }];
+
+export function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -34,6 +38,8 @@ export function addToCart(productId) {
   setTimeout(() =>{
     addedElement.classList.remove('js-added');
   }, 2000)
+
+  saveToStorage()
 };
 
 export function removeFromCart(productId) {
@@ -46,4 +52,6 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
 };
